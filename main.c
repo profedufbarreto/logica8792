6ce97124bbf0f4194a3e436d8ae78185621f404e
profedubarreto <01289211027@senacrs.edu.br>
2026-09-09 21:34:30 -0300

    soma, sub, mult e div

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 #include<stdio.h>
+#include<windows.h>
 
 int main(){
+
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
 
     int a = 5;
     int b = 4;
 
-    printf("O maior número é: %d\n", (a > b) ? a : b);
+    printf("A soma é: %d\n", (a + b));
+    printf("A subtração é: %d\n", (a - b));
+    printf("A multiplicação é: %d\n", (a * b));
+    printf("A divisão é: %d\n", (a / b));
+
+    
 
     return 0;
 }
