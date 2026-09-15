696c84984e12f42adb28c760ff46563f0a1ca09f
profedubarreto <01289211027@senacrs.edu.br>
2026-09-14 21:59:42 -0300

    and

--- main.c
+++ main.c
@@ -6,15 +6,17 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int a, b;
+    int a, b, c;
 
     printf("Digite dois números: ");
-    scanf("%d %d", &a, &b);
+    scanf("%d %d %d", &a, &b, &c);
 
-    if(a == b){
-        printf("Os números são iguais!\n");
+    if((a != b) && (b != c)){
+        printf("Os três números são diferentes!\n");
+    }else if(b != c){
+        printf("Os dois números são diferentes!\n");
     }else{
-        printf("Os números são diferentes!\n");
+        printf("Todos os números são iguais.");
     }
 
     return 0;
