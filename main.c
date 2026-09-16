da9d3f68e327c9568b3dd2a531b993fea1dcd372
profedubarreto <01289211027@senacrs.edu.br>
2026-09-16 19:57:01 -0300

    nota e frequencia

--- main.c
+++ main.c
@@ -6,18 +6,28 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int idade;
-    int ehEstudante;
+    int n1, n2, n3;
+    float media;
+    float freq;
 
-    printf("Qual sua idade?");
-    scanf("%d", &idade);
-    printf("\nVocê é estudante (0 - Não | 1 - Sim):");
-    scanf("%d", &ehEstudante);
+    printf("Digite a nota 1: ");
+    scanf("%d", &n1);
+    
+    printf("\nDigite a nota 2: ");
+    scanf("%d", &n2);
 
-    if((idade >= 60) && (ehEstudante == 1)){
-        printf("Parabéns, você tem direito a desconto!");
+    printf("\nDigite a nota 3: ");
+    scanf("%d", &n3);
+
+    media = (n1 + n2 + n3) / 3;
+
+    printf("Digite a frequência (em números apenas): ");
+    scanf("%f", &freq);
+
+    if(media >= 7 && freq >= 75){
+        printf("\nAprovado com sucesso!");
     }else{
-        printf("Infelizmente você não terá desconto!");
+        printf("\nReprovado!");
     }
 
     return 0;
