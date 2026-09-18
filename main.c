c0db644fd5826354c6f9a829d64a3b16363dcadc
profedubarreto <01289211027@senacrs.edu.br>
2026-09-18 20:56:59 -0300

    vogal

--- main.c
+++ main.c
@@ -5,34 +5,16 @@ int main(){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    int opcao;
+    char letra;
+    printf("Digite uma letra: ");
+    scanf("%c", &letra);
 
-    printf("--- SISTEMA DE PEDÁGIO ---\n");
-    printf("1 - Moto\n");
-    printf("2 - Carro Passeio\n");
-    printf("3 - Caminhão\n");
-    printf("4 - Ônibus\n");
-    scanf("%d", &opcao);
-
-    switch (opcao)
-    {
-    case 1:
-        printf("Categoria: Moto | Tarifa: R$ 5,00\n");
-        break;
-    case 2:
-        printf("Categoria: Carro Passeio | Tarifa: R$ 10,00\n");
-        break;
-    case 3:
-        printf("Categoria: Caminhão | Tarifa: R$ 25,00\n");
-        break;
-    case 4:
-        printf("Categoria: Ônibus | Tarifa: R$ 30,00\n");
-        break;
-    default:
-        printf("Erro: Categoria não cadastrada no sistema.\n");
-        break;
-    }
-    
+    if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' ||
+        letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U'){
+            printf("Vogal\n");
+        }else{
+            printf("Consoante\n");
+        }
 
     return 0;
 }
