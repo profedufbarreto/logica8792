6a80ce36d6d51cd0d5072d2ef7465ad3da5fab89
profedubarreto <01289211027@senacrs.edu.br>
2026-09-18 19:40:27 -0300

    Sistema de tarifas deveículos

--- main.c
+++ main.c
@@ -10,6 +10,8 @@ int main(){
     printf("--- SISTEMA DE PEDÁGIO ---\n");
     printf("1 - Moto\n");
     printf("2 - Carro Passeio\n");
+    printf("3 - Caminhão\n");
+    printf("4 - Ônibus\n");
     scanf("%d", &opcao);
 
     switch (opcao)
@@ -19,6 +21,12 @@ int main(){
         break;
     case 2:
         printf("Categoria: Carro Passeio | Tarifa: R$ 10,00\n");
+        break;
+    case 3:
+        printf("Categoria: Caminhão | Tarifa: R$ 25,00\n");
+        break;
+    case 4:
+        printf("Categoria: Ônibus | Tarifa: R$ 30,00\n");
         break;
     default:
         printf("Erro: Categoria não cadastrada no sistema.\n");
