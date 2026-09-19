81b59fa18790489c2a3b5510a4c80eea08990f06
profedubarreto <01289211027@senacrs.edu.br>
2026-09-18 21:48:22 -0300

    imc

--- main.c
+++ main.c
@@ -5,18 +5,27 @@ int main(){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    int n;
-    printf("Digite um número: ");
-    scanf("%d", &n);
+    float peso, altura, imc;
 
-    if(n % 3 == 0 && n % 5 == 0){
-        printf("Múltiplo de 3 e 5\n");
-    }else if(n % 3 == 0){
-        printf("Múltiplo de 3\n");
-    }else if(n % 5 == 0){
-        printf("Múltiplo de 5\n");
+    printf("Digite o peso (kg): ");
+    scanf("%f", &peso);
+
+    printf("Digite a altura (m): ");
+    scanf("%f", &altura);
+
+    imc = peso / (altura * altura);
+
+    if(imc < 18.5){
+        printf("Seu peso é: %f\n", peso);
+        printf("Sua altura é: %f\n", altura);
+        printf("Classificação: Abaixo do peso!\n");
+    }else if(imc < 25){
+        printf("Classificação: Peso normal!\n");
+    }else if(imc < 30){
+        printf("Classificação: Sobrepeso!\n");
     }else{
-        printf("Não é múltiplo de 3 nem de 5\n");
+        printf("Classificação: Obesidade!\n");
     }
+
     return 0;
 }
