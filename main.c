f72f37c7a3da771cb6c2255b3233d57c24d150ef
profedubarreto <01289211027@senacrs.edu.br>
2026-09-18 21:11:00 -0300

    múltiplos

--- main.c
+++ main.c
@@ -5,16 +5,18 @@ int main(){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    char letra;
-    printf("Digite uma letra: ");
-    scanf("%c", &letra);
+    int n;
+    printf("Digite um número: ");
+    scanf("%d", &n);
 
-    if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' ||
-        letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U'){
-            printf("Vogal\n");
-        }else{
-            printf("Consoante\n");
-        }
-
+    if(n % 3 == 0 && n % 5 == 0){
+        printf("Múltiplo de 3 e 5\n");
+    }else if(n % 3 == 0){
+        printf("Múltiplo de 3\n");
+    }else if(n % 5 == 0){
+        printf("Múltiplo de 5\n");
+    }else{
+        printf("Não é múltiplo de 3 nem de 5\n");
+    }
     return 0;
 }
