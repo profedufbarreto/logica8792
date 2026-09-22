bd41d0058b0b68e392a1f09cd21e68369355abe4
profedubarreto <01289211027@senacrs.edu.br>
2026-09-22 19:41:37 -0300

    função com par..

--- main.c
+++ main.c
@@ -2,15 +2,17 @@
 #include<locale.h>
 #include<string.h>
 
-void saudar(){
-    printf("Olá mundo!\n");
+void saudar(const char *nome){
+    printf("Olá %s\n", nome);
 }
 
 int main(){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    saudar();
+    saudar("Eduardo");
+    saudar("Filippsen");
+    saudar("Barreto");
 
     return 0;
 }
