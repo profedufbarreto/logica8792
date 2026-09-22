f6b8ccbccb1ac7d46b975b021916b8c20f0bbd82
profedubarreto <01289211027@senacrs.edu.br>
2026-09-21 21:43:42 -0300

    troca de  valor do array

--- main.c
+++ main.c
@@ -6,19 +6,13 @@ int main(){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    int soma = 0;
-    int i;
+    int numeros[] = {1, 2, 3};
 
-    for(i = 1; i <= 5; i++){
-        //0 + 1
-        //1 + 2
-        //3 + 3
-        //6 + 4
-        //10 + 5
-        soma = soma + i;
-    }
-    
-    printf("Soma é %d", soma);
+    numeros[0] = 10;
+
+    printf("%d", numeros[0]);
+
+
 
     return 0;
 }
