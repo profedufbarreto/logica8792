2a6adf6ea63db2069f08334a4da5ed6016af7543
profedubarreto <01289211027@senacrs.edu.br>
2026-09-22 19:25:17 -0300

    ponteiro

--- main.c
+++ main.c
@@ -2,16 +2,17 @@
 #include<locale.h>
 #include<string.h>
 
+void dobrar(int *x){
+    *x = *x * 2;
+}
+
 int main(){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    int x = 10;
-    int *p = &x;
-
-    printf("Valor de x: %d\n", x);
-    printf("Endereço de x: %p\n", &x);
-    printf("Valor via ponteiro: %d\n", *p);
+    int numero = 10;
+    dobrar(&numero);
+    printf("Número depois da função %d\n", numero);
 
     return 0;
 }
