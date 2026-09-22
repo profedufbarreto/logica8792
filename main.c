6a944e34397949ef326eecf52f137e6cab1813be
profedubarreto <01289211027@senacrs.edu.br>
2026-09-21 21:10:45 -0300

    for + 2

--- main.c
+++ main.c
@@ -6,11 +6,9 @@ int main(){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    for(int i = 0; i < 10; i++){
-        printf("%s\n %d", "\nOs números estão aumentando: --->>" ,i);
+    for(int i = 0; i <= 10; i = i + 2){
+        printf("%d\n", i);
     }
-
-    printf("\nMeu For funcionou!!");
 
     return 0;
 }
