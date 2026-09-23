7f0e43fc19b2219a2c5ad58aeec34a11797a2718
profedubarreto <01289211027@senacrs.edu.br>
2026-09-22 21:27:26 -0300

    relógio com for

--- main.c
+++ main.c
@@ -6,10 +6,22 @@ int main(){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    int num[5] = {10, 20, 30, 40, 50};
+    int horas[12];
+    int minutos[60];
 
-    for(int i = 0; i < 5; i++){
-        printf("%d\n", num[i]);
+    for(int i = 0; i < 12; i++){
+        horas[i] = i + 1;
+    }
+
+    for(int j = 0; j < 60; j++){
+        minutos[j] = j;
+    }
+
+    //simula o relógio
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 60; j++){
+            printf("Hora: %2d:%02d\n", horas[i], minutos[j]);
+        }
     }
 
     return 0;
