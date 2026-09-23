ae6ec4f260e27c4eb629e01e1d817b263c2e6dbf
profedubarreto <01289211027@senacrs.edu.br>
2026-09-23 20:41:44 -0300

    input e função

--- main.c
+++ main.c
@@ -2,18 +2,18 @@
 #include<locale.h>
 
 int tabuada(int n){
-    int resultado;
     printf("Tabuada do %d:\n", n);
     for(int i = 1; i <= 10; i++){
-        resultado = n * i;
-        printf("%d x %d = %d\n", n, i, resultado);
+        printf("%d x %d = %d\n", n, i, n * i);
     }
-    return resultado;
+    return 0;
 }
 int main(){
     setlocale(LC_ALL, "pt_BR.UTF-8");
-    int ultimo = tabuada(7);
-    printf("Último valor da tabuada: %d\n", ultimo);
+    int numero;
+    printf("Digite um número para ver a tabuada: ");
+    scanf("%d", &numero);
+    tabuada(numero);
     
 
     return 0;
