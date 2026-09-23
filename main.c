70dae95b31766e4f7b91519e6486572a85255991
profedubarreto <01289211027@senacrs.edu.br>
2026-09-23 20:12:38 -0300

    tabuada com função int

--- main.c
+++ main.c
@@ -1,17 +1,20 @@
 #include<stdio.h>
 #include<locale.h>
 
-void tabuada(int n){
-    printf("Tabuada do %d\n", n);
+int tabuada(int n){
+    int resultado;
+    printf("Tabuada do %d:\n", n);
     for(int i = 1; i <= 10; i++){
-        printf("%d x %d = %d\n", n, i, n * i);
+        resultado = n * i;
+        printf("%d x %d = %d\n", n, i, resultado);
     }
+    return resultado;
 }
-
 int main(){
     setlocale(LC_ALL, "pt_BR.UTF-8");
+    int ultimo = tabuada(7);
+    printf("Último valor da tabuada: %d\n", ultimo);
     
-    tabuada(7);
 
     return 0;
 }
