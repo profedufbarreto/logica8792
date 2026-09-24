42f39480b9a456aa829f2072774bee5259c5f7c5
profedubarreto <01289211027@senacrs.edu.br>
2026-09-24 20:10:51 -0300

    while

--- main.c
+++ main.c
@@ -1,20 +1,18 @@
 #include<stdio.h>
 #include<locale.h>
 
-void maxMin(int v[], int tamanho){
-    int max = v[0], min = v[0];
-    for(int i = 1; i < tamanho; i++){
-        if(v[i] > max) max = v[i];
-        if(v[i] < min) min = v[i];
-    }
-    printf("Máximo: %d, Mínimo: %d\n", max, min);
-}
-
 int main(){
     setlocale(LC_ALL, "pt_BR.UTF-8");
     
-    int numeros[] = {4, 8, 2, 15, 6};
-    maxMin(numeros, 5);
+    int numero;
+    printf("Digite um número positivo: ");
+    scanf("%d", &numero);
+
+    while(numero <= 0){
+        printf("Número inválido! Digite novamente: ");
+        scanf("%d", &numero);
+    }
+    printf("Número válido: %d\n", numero);
 
     return 0;
 }
