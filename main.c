f100a82793fc174fd69c9efe94b0dcaac0e2745e
profedubarreto <01289211027@senacrs.edu.br>
2026-09-23 21:00:26 -0300

    verificar é par ou ímpar

--- main.c
+++ main.c
@@ -1,20 +1,19 @@
 #include<stdio.h>
 #include<locale.h>
 
-int tabuada(int n){
-    printf("Tabuada do %d:\n", n);
-    for(int i = 1; i <= 10; i++){
-        printf("%d x %d = %d\n", n, i, n * i);
+void verificarEhParImpar(int n){
+    if(n % 2 == 0){
+        printf("%d é par!\n", n);
+    }else{
+        printf("%d é ímpar\n", n);
     }
-    return 0;
 }
+
 int main(){
     setlocale(LC_ALL, "pt_BR.UTF-8");
-    int numero;
-    printf("Digite um número para ver a tabuada: ");
-    scanf("%d", &numero);
-    tabuada(numero);
     
+    verificarEhParImpar(10);
+    verificarEhParImpar(7);
 
     return 0;
 }
